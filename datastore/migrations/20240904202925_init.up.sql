156289300967c3b6a9acb9cbfc6e66175303c7e7
Darnell Andries <dandries@brave.com>
2024-10-09 13:25:23 -0700

    Reorder columns for data alignment

--- datastore/migrations/20240904202925_init.up.sql
+++ datastore/migrations/20240904202925_init.up.sql
@@ -1,36 +1,36 @@
 CREATE TABLE chains (
 	id BIGSERIAL PRIMARY KEY,
-	client_id BYTEA NOT NULL,
 	last_usage_time TIMESTAMP NOT NULL,
+	client_id BYTEA NOT NULL,
 	UNIQUE (client_id)
 );
 
 CREATE TABLE dynamo_migration_statuses (
 	chain_id BIGINT REFERENCES chains(id) ON DELETE CASCADE,
-	data_type INTEGER,
 	-- null earliest_mtime indicates that all entities have been migrated
 	earliest_mtime BIGINT,
+	data_type INTEGER,
 	PRIMARY KEY (chain_id, data_type)
 );
 
 CREATE TABLE entities (
 	id UUID,
 	chain_id BIGINT NOT NULL REFERENCES chains(id) ON DELETE CASCADE,
-	data_type INTEGER NOT NULL,
 	ctime BIGINT NOT NULL,
 	mtime BIGINT NOT NULL,
+	version BIGINT NOT NULL,
+	data_type INTEGER NOT NULL,
 	specifics BYTEA STORAGE EXTERNAL NOT NULL,
-	deleted BOOL NOT NULL,
 	client_defined_unique_tag TEXT STORAGE PLAIN,
 	server_defined_unique_tag TEXT STORAGE PLAIN,
-	folder BOOLEAN,
-	version BIGINT NOT NULL,
 	name TEXT STORAGE PLAIN,
 	originator_cache_guid TEXT STORAGE PLAIN,
 	originator_client_item_id TEXT STORAGE PLAIN,
 	parent_id TEXT STORAGE PLAIN,
 	non_unique_name TEXT STORAGE PLAIN,
 	unique_position BYTEA STORAGE PLAIN,
+	folder BOOLEAN,
+	deleted BOOLEAN NOT NULL,
 	PRIMARY KEY (id, chain_id),
 	UNIQUE (chain_id, client_defined_unique_tag)
 );
