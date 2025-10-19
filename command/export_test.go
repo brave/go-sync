322f81ec27d13c7b0c73e3c950255045a563fc3b
mschfh <37435502+mschfh@users.noreply.github.com>
2025-10-18 23:17:34 -0500

    fmt

--- command/export_test.go
+++ command/export_test.go
@@ -1,19 +1,19 @@
 package command
 
 var (
-	NigoriName                 = nigoriName
-	NigoriTag                  = nigoriTag
-	BookmarksName              = bookmarksName
-	BookmarksTag               = bookmarksTag
-	OtherBookmarksName         = otherBookmarksName
-	OtherBookmarksTag          = otherBookmarksTag
-	SyncedBookmarksName        = syncedBookmarksName
-	SyncedBookmarksTag         = syncedBookmarksTag
-	BookmarkBarName            = bookmarkBarName
-	BookmarkBarTag             = bookmarkBarTag
-	StoreBirthday              = storeBirthday
-	MaxCommitBatchSize         = maxCommitBatchSize
-	SetSyncPollInterval        = setSyncPollInterval
-	MaxGUBatchSize             = &maxGUBatchSize
-	MaxClientObjectQuota       = &maxClientObjectQuota
+	NigoriName           = nigoriName
+	NigoriTag            = nigoriTag
+	BookmarksName        = bookmarksName
+	BookmarksTag         = bookmarksTag
+	OtherBookmarksName   = otherBookmarksName
+	OtherBookmarksTag    = otherBookmarksTag
+	SyncedBookmarksName  = syncedBookmarksName
+	SyncedBookmarksTag   = syncedBookmarksTag
+	BookmarkBarName      = bookmarkBarName
+	BookmarkBarTag       = bookmarkBarTag
+	StoreBirthday        = storeBirthday
+	MaxCommitBatchSize   = maxCommitBatchSize
+	SetSyncPollInterval  = setSyncPollInterval
+	MaxGUBatchSize       = &maxGUBatchSize
+	MaxClientObjectQuota = &maxClientObjectQuota
 )
