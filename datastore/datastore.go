fd650925e414cd4cc9194fa3fafc9f3983252139
mschfh <37435502+mschfh@users.noreply.github.com>
2026-02-16 17:45:07 -0500

    chore(lint): apply golines formatting

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -14,13 +14,25 @@ type Datastore interface {
 	// client token for a given client. Besides the array of sync entities, a
 	// boolean value indicating whether there are more updates to query in the
 	// next batch is returned.
-	GetUpdatesForType(ctx context.Context, dataType int, clientToken int64, fetchFolders bool, clientID string, maxSize int64) (bool, []SyncEntity, error)
+	GetUpdatesForType(
+		ctx context.Context,
+		dataType int,
+		clientToken int64,
+		fetchFolders bool,
+		clientID string,
+		maxSize int64,
+	) (bool, []SyncEntity, error)
 	// Check if a server-defined unique tag is in the datastore.
 	HasServerDefinedUniqueTag(ctx context.Context, clientID string, tag string) (bool, error)
 	// Get the count of sync items for a client.
 	GetClientItemCount(ctx context.Context, clientID string) (*ClientItemCounts, error)
 	// Update the count of sync items for a client.
-	UpdateClientItemCount(ctx context.Context, counts *ClientItemCounts, newNormalItemCount int, newHistoryItemCount int) error
+	UpdateClientItemCount(
+		ctx context.Context,
+		counts *ClientItemCounts,
+		newNormalItemCount int,
+		newHistoryItemCount int,
+	) error
 	// ClearServerData deletes all items for a given clientID
 	ClearServerData(ctx context.Context, clientID string) ([]SyncEntity, error)
 	// DisableSyncChain marks a chain as disabled so no further updates or commits can happen
