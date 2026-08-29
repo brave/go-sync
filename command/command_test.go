04575ce7991505342d0414e9c9f0f230134b236a
mschfh <37435502+mschfh@users.noreply.github.com>
2026-08-28 19:46:50 -0500

    chore(lint): Ignore unparam on expectedChangesRemaining

--- command/command_test.go
+++ command/command_test.go
@@ -194,7 +194,7 @@ func assertCommonResponse(suite *CommandTestSuite, rsp *sync_pb.ClientToServerRe
 
 func assertGetUpdatesResponse(suite *CommandTestSuite, rsp *sync_pb.GetUpdatesResponse,
 	newMarker *[]*sync_pb.DataTypeProgressMarker, expectedPBSyncAttrs []*PBSyncAttrs,
-	expectedChangesRemaining int64) { //nolint:unparam
+	expectedChangesRemaining int64) { //nolint:unparam // kept for tests that expect a non-zero remaining count
 	pbSyncAttrs := []*PBSyncAttrs{}
 	for _, entity := range rsp.Entries {
 		// Update tokens in the expected NewProgressMarker
